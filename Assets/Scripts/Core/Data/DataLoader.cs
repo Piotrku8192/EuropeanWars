308d8f14a687f5e215604a2dc372d51aa4cb195a
Piotrku8192 <pszulakowski321@gmail.com>
2020-07-10 13:38:11 +0200

    Rewrite DataLoader

--- Assets/Scripts/Core/Data/DataLoader.cs
+++ Assets/Scripts/Core/Data/DataLoader.cs
@@ -17,6 +17,7 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using EuropeanWars.Core.Army;
+using System.Threading;
 
 namespace EuropeanWars.Core.Data {
     public class DataLoader : MonoBehaviour
@@ -30,124 +31,82 @@ namespace EuropeanWars.Core.Data {
         public GameObject loadingScreen;
         public Image progressBar;
         public Text progressText;
-        public int steps;
+        private bool loaded;
+
+        private bool convertFromJsonFiles = false;
+        private Thread loadingThread;
 
         public void Start() {
             path = Application.dataPath + "\\Content";
 #if UNITY_EDITOR
             path = Directory.GetCurrentDirectory() + "\\Tests\\Content";
+            convertFromJsonFiles = true;
 #endif
+            loadingThread = new Thread(LoadData);
+            loadingThread.Start();
 
-            UnitData[] u = JsonConvert.DeserializeObject<UnitData[]>(File.ReadAllText(path + "\\units.json"));
-            gameData.units = new UnitData[u.Length];
-            for (int i = 0; i < u.Length; i++) {
-                gameData.units[i] = u[i];
-            }
-            File.WriteAllText(path + "\\units", DataConverter.ToString(gameData.units));
-
-            //LanguageDictionary.languages = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(File.ReadAllText(path + "\\language.json"));
-            //File.WriteAllText(path + "\\language", DataConverter.ToString(LanguageDictionary.languages));
-            StartCoroutine(LoadContent());
         }
 
-        public IEnumerator LoadContent() {
-            yield return new WaitForEndOfFrame();
+        public void Update() {
+            //UpdateProgressBar();
+            if (loaded) {
+                foreach (var item in gameData.gfx) {
+                    Texture2D tex = new Texture2D(2, 2);
+                    tex.LoadImage(item.Value);
+                    GameInfo.gfx.Add(item.Key, Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2()));
+                }
 
-            UpdateProgressBar(0, "Wczytywanie języków...");
-            yield return new WaitForEndOfFrame();
-            LanguageDictionary.languages = DataConverter.FromString<List<Dictionary<string, string>>>(File.ReadAllText(path + "\\language"));
-            LanguageDictionary.language = LanguageDictionary.languages[0];
-            //////////////////////////////////////////////////////////////////////////////////////
+                MapData m = DataConverter.FromJson<MapData>(gameData.map);
+                MapGenerator.Singleton.mapData = m;
+                MapGenerator.Singleton.GenerateMap();
 
-            UpdateProgressBar(1, "Wczytywanie grafik...");
-            yield return new WaitForEndOfFrame();
+                GameInfo.Initialize();
+
+                NetOutgoingMessage msg = Client.Singleton.c.CreateMessage();
+                msg.Write((ushort)1);
+                Client.Singleton.c.SendMessage(msg, NetDeliveryMethod.ReliableOrdered);
+
+                loadingScreen.SetActive(false);
+                loaded = false;
+            }
+        }
+
+        private void LoadData() {
+            if (convertFromJsonFiles) {
+                ConvertFromJsonFiles();
+            }
+            gameData = DataConverter.FromString<GameData>(File.ReadAllText(path + "\\gameData"));
+            gameData.FillGameInfo();
+
+            loaded = true;
+            loadingThread.Abort();
+        }
+
+        private void ConvertFromJsonFiles() {
             foreach (var item in Directory.GetFiles(path + "\\gfx")) {
                 string name = item.Split('\\').Last().Split('.').First();
                 byte[] bytes = File.ReadAllBytes(item);
-                Texture2D tex = new Texture2D(2, 2);
-                tex.LoadImage(bytes);
-                GameInfo.gfx.Add(name, Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2()));
+                gameData.gfx.Add(name, bytes);
             }
-            //////////////////////////////////////////////////////////////////////////////////////
-
-            UpdateProgressBar(2, "Wczytywanie budynków...");
-            yield return new WaitForEndOfFrame();
-            gameData.buildings = DataConverter.FromString<BuildingData[]>(File.ReadAllText(path + "\\buildings"));
-            foreach (var item in gameData.buildings) {
-                GameInfo.buildings.Add(item.id, new BuildingInfo(item));
-            }
-            //////////////////////////////////////////////////////////////////////////////////////
-
-            UpdateProgressBar(3, "Wczytywanie religii...");
-            yield return new WaitForEndOfFrame();
-            gameData.religions = DataConverter.FromString<ReligionData[]>(File.ReadAllText(path + "\\religions"));
-            foreach (var item in gameData.religions) {
-                GameInfo.religions.Add(item.id, new ReligionInfo(item));
-            }
-            //////////////////////////////////////////////////////////////////////////////////////
-
-            UpdateProgressBar(4, "Wczytywanie kultur...");
-            yield return new WaitForEndOfFrame();
-            gameData.cultures = DataConverter.FromString<CultureData[]>(File.ReadAllText(path + "\\cultures"));
-            foreach (var item in gameData.cultures) {
-                GameInfo.cultures.Add(item.id, new CultureInfo(item));
-            }
-            //////////////////////////////////////////////////////////////////////////////////////
-
-            UpdateProgressBar(5, "Wczytywanie państw...");
-            yield return new WaitForEndOfFrame();
-            gameData.countries = DataConverter.FromString<CountryData[]>(File.ReadAllText(path + "\\countries"));
-            foreach (var item in gameData.countries) {
-                GameInfo.countries.Add(item.id, new CountryInfo(item));
-            }
-            //////////////////////////////////////////////////////////////////////////////////////
-
-            UpdateProgressBar(6, "Wczytywanie prowincji...");
-            yield return new WaitForEndOfFrame();
-            gameData.provinces = DataConverter.FromString<ProvinceData[]>(File.ReadAllText(path + "\\provinces"));
-            int i = 0;
-            foreach (var item in gameData.provinces) {
-                item.id = i;
-                GameInfo.provinces.Add(item.id, new ProvinceInfo(item));
-                i++;
-            }
-            //File.WriteAllText(path + "\\provinces", DataConverter.ToString(gameData.provinces));
-            //////////////////////////////////////////////////////////////////////////////////////
-
-            UpdateProgressBar(6, "Wczytywanie jednostek wojskowych...");
-            yield return new WaitForEndOfFrame();
-            gameData.units = DataConverter.FromString<UnitData[]>(File.ReadAllText(path + "\\units"));
-            foreach (var item in gameData.units) {
-                GameInfo.units.Add(item.id, new UnitInfo(item));
-            }
-            //////////////////////////////////////////////////////////////////////////////////////
-            
-            UpdateProgressBar(7, "Wczytywanie mapy...");
-            yield return new WaitForEndOfFrame();
-            map = DataConverter.FromJson<MapData>(File.ReadAllText(path + "\\map"));
-            //////////////////////////////////////////////////////////////////////////////////////
-            
-            UpdateProgressBar(8, "Generowanie mapy...");
-            yield return new WaitForEndOfFrame();
-            generator.mapData = map;
-            generator.GenerateMap();
-            //////////////////////////////////////////////////////////////////////////////////////
-
-            UpdateProgressBar(9, "Initializacja obiektów...");
-            yield return new WaitForEndOfFrame();
-            GameInfo.Initialize();
-            //////////////////////////////////////////////////////////////////////////////////////
-
-            NetOutgoingMessage msg = Client.Singleton.c.CreateMessage();
-            msg.Write((ushort)1);
-            Client.Singleton.c.SendMessage(msg, NetDeliveryMethod.ReliableOrdered);
-
-            loadingScreen.SetActive(false);
+            gameData.languages = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(File.ReadAllText(path + "\\languages.json"));
+            gameData.buildings = JsonConvert.DeserializeObject<BuildingData[]>(File.ReadAllText(path + "\\buildings.json"));
+            gameData.religions = JsonConvert.DeserializeObject<ReligionData[]>(File.ReadAllText(path + "\\religions.json"));
+            gameData.cultures = JsonConvert.DeserializeObject<CultureData[]>(File.ReadAllText(path + "\\cultures.json"));
+            gameData.countries = JsonConvert.DeserializeObject<CountryData[]>(File.ReadAllText(path + "\\countries.json"));
+            gameData.provinces = JsonConvert.DeserializeObject<ProvinceData[]>(File.ReadAllText(path + "\\provinces.json"));
+            gameData.units = JsonConvert.DeserializeObject<UnitData[]>(File.ReadAllText(path + "\\units.json"));
+            gameData.map = File.ReadAllText(path + "\\map");
+            File.WriteAllText(path + "\\gameData", DataConverter.ToString(gameData));
         }
 
-        public void UpdateProgressBar(int step, string text) {
-            progressBar.fillAmount = (float)step / steps;
-            progressText.text = text;
+        private void ConvertToJsonFiles() {
+            File.WriteAllText(path + "\\languages.json", JsonConvert.SerializeObject(LanguageDictionary.languages));
+            File.WriteAllText(path + "\\buildings.json", JsonConvert.SerializeObject(gameData.buildings));
+            File.WriteAllText(path + "\\religions.json", JsonConvert.SerializeObject(gameData.religions));
+            File.WriteAllText(path + "\\cultures.json", JsonConvert.SerializeObject(gameData.cultures));
+            File.WriteAllText(path + "\\countries.json", JsonConvert.SerializeObject(gameData.countries));
+            File.WriteAllText(path + "\\provinces.json", JsonConvert.SerializeObject(gameData.provinces));
+            File.WriteAllText(path + "\\units.json", JsonConvert.SerializeObject(gameData.units));
         }
     }
 }
--- Assets/Scripts/Core/Data/GameData.cs
+++ Assets/Scripts/Core/Data/GameData.cs
@@ -1,13 +1,59 @@
-﻿using System;
+﻿using EuropeanWars.Core.Army;
+using EuropeanWars.Core.Building;
+using EuropeanWars.Core.Country;
+using EuropeanWars.Core.Culture;
+using EuropeanWars.Core.Language;
+using EuropeanWars.Core.Province;
+using EuropeanWars.Core.Religion;
+using EuropeanWars.GameMap;
+using EuropeanWars.GameMap.Data;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace EuropeanWars.Core.Data {
     [Serializable]
     public class GameData {
+        public Dictionary<string, byte[]> gfx = new Dictionary<string, byte[]>();
+        public List<Dictionary<string, string>> languages = new List<Dictionary<string, string>>();
         public ProvinceData[] provinces;
         public CountryData[] countries;
         public ReligionData[] religions;
         public CultureData[] cultures;
         public BuildingData[] buildings;
         public UnitData[] units;
+        public string map;
+
+        public void FillGameInfo() {
+            LanguageDictionary.languages = languages;
+            LanguageDictionary.language = languages[0];
+            int i = 0;
+
+            foreach (var item in buildings) {
+                GameInfo.buildings.Add(item.id, new BuildingInfo(item));
+            }
+
+            foreach (var item in religions) {
+                GameInfo.religions.Add(item.id, new ReligionInfo(item));
+            }
+
+            foreach (var item in cultures) {
+                GameInfo.cultures.Add(item.id, new CultureInfo(item));
+            }
+
+            foreach (var item in countries) {
+                GameInfo.countries.Add(item.id, new CountryInfo(item));
+            }
+
+            foreach (var item in provinces) {
+                item.id = i;
+                GameInfo.provinces.Add(item.id, new ProvinceInfo(item));
+                i++;
+            }
+
+            foreach (var item in units) {
+                GameInfo.units.Add(item.id, new UnitInfo(item));
+            }
+        }
     }
 }
